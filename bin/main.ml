386933362605fcb4d50e7b5faa8e8372366f68fc
abm247 <abl247@cornell.edu>
2024-03-28 16:12:38 -0400

    Fixed 1 error

--- bin/main.ml
+++ bin/main.ml
@@ -2,11 +2,11 @@ open Gtk
 
 let create_window () =
   let window = GWindow.window ~title:"Sudoku" ~width:300 ~height:300 () in
-  window#connect#destroy ~callback:Main.quit;
+  ignore (window#connect#destroy ~callback:GMain.Main.quit);
   window
 
 let create_grid () =
-  let grid = GPack.grid ~rows:9 ~columns:9 ~row_spacing:2 ~column_spacing:2 () in
+  let grid = GtkGrid.grid ~columns:9 ~rows:9 ~row_spacing:2 ~column_spacing:2 () in
   for i = 0 to 8 do
     for j = 0 to 8 do
       let entry = GEdit.entry ~width_chars:1 ~max_length:1 () in
