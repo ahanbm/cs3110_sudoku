4c6e29b0d5b339af3140f04e783b1618399c132b
PeterFavero <pmf66@cornell.edu>
2024-04-20 13:28:02 -0400

    begin implementing coloring of cells

--- bin/main.ml
+++ bin/main.ml
@@ -3,9 +3,6 @@
    dune exec ./bin/main.exe 
   in the terminal *)
 
-(* Initialize a 9x9 array with zeros *)
-let sudoku_grid = Array.make_matrix 9 9 0;;
-
 let welcome_user = print_endline 
 "
 Welcome to CS3110_sudoku!
@@ -23,51 +20,90 @@ You'll enter input in the format \'<row> <col> <number>\' to update the board.
 You should enter <row>, <col>, and <number> as digits 1-9.
 The first row is the top row, and the first column is the leftmost column. 
 If there is a formatting error, the game will help you correct it but won't count that move. 
-You can enter \'help\' to recieve helpful formatting tips at any time.
+You can enter \'help\' to recieve helpful formatting tips at any time, which also won't count towards your moves.
 Good luck! You've got this! Happy sudoku-solving!
 ";;
+
+
+(* Type for the set of cells we use to check wether a value can be changed *)
+module Cell = struct
+  type t = int * int
+  let compare = compare
+end
+
+module CellSet = Set.Make(Cell);;
+
+(* Initialize the set of cells and grid with one immutable value for testing *)
+let immutable_cells = CellSet.empty;;
+let immutable_cells = CellSet.add (2,2) immutable_cells;;
+
+let sudoku_grid = Array.make_matrix 9 9 (0, "none");;
+sudoku_grid.(2).(2) <- (9, "blue")
+
+
 
 let clear_line () =
   output_string stdout "\027[A\027[2K";
   flush stdout
 
-let rec get_input () = 
+let rec get_input immutable_cells = 
   let input_string = read_line () in
   let regex = Str.regexp "^\\([1-9]\\) \\([1-9]\\) \\([1-9]\\)$" in
   if Str.string_match regex input_string 0 then
     let row = int_of_string (Str.matched_group 1 input_string) in
     let col = int_of_string (Str.matched_group 2 input_string) in
     let number = int_of_string (Str.matched_group 3 input_string) in
-    let () = clear_line () in
-    let () = print_endline ("Move : " ^ input_string ^ "\n") in 
-    (row, col, number)
-  else let () = clear_line () in get_input ()
+    if CellSet.mem (row-1, col-1) immutable_cells then 
+      let () = clear_line () in get_input immutable_cells
+    else 
+      let () = clear_line () in
+      let () = print_endline ("Move : " ^ input_string ^ "\n") in 
+      (row, col, number)
+  else let () = clear_line () in get_input immutable_cells
+
 
 (* This code was written using information from GPT-4 *)
 (* Function to print the sudoku grid *)
 let print_sudoku_grid grid =
   for i = 0 to 8 do
-    (* Print horizontal dividers *)
     if i mod 3 = 0 then print_endline "-------------------------";
     for j = 0 to 8 do
-      (* Print vertical dividers *)
       if j mod 3 = 0 then print_string "| ";
-      (* Print cell value or space for zero *)
-      if grid.(i).(j) = 0 then print_string ". "
-      else Printf.printf "%d " grid.(i).(j);
+      let (value, color) = grid.(i).(j) in
+      match color with
+      | "red" -> (* error in the current group *)
+          if value = 0 then 
+            Printf.printf "\027[31m. \027[0m"
+          else 
+            Printf.printf "\027[31m%d \027[0m" value
+      | "green" -> (* current group is full and without error *)
+          if value = 0 then 
+            Printf.printf "\027[32m. \027[0m"
+          else 
+            Printf.printf "\027[32m%d \027[0m" value
+      | "blue" -> (* current value is immutable *)
+          if value = 0 then 
+            Printf.printf "\027[34m. \027[0m"
+          else 
+            Printf.printf "\027[34m%d \027[0m" value
+      | _ ->  (* Treat all other colors as "none", butn only "none" should be used. *)
+          if value = 0 then 
+            print_string ". "
+          else 
+            Printf.printf "%d " value
     done;
     print_endline "|";
   done;
   print_endline "-------------------------";;
 
-let rec run_game (sudoku_grid : int array array) (grid_solved : bool) (move_count : int) =
+let rec run_game (sudoku_grid : (int*string) array array) (grid_solved : bool) (move_count : int) =
   match grid_solved with 
   | true -> print_endline ("Congrats, you won!\nYou solved this sudoku puzzle in " ^ (string_of_int move_count) ^ " moves!\nGreat job! We encourage you to play another exciting game of sudoku.\n");
   | false -> 
-    print_endline ("Sudoku Board | Move " ^ (string_of_int move_count) ^ " :");
+    print_endline ("Sudoku Board | Move " ^ (string_of_int (move_count-1)) ^ " :");
     print_sudoku_grid sudoku_grid;
-    let (row, col, number) = get_input () in
-    sudoku_grid.(row-1).(col-1) <- number;
+    let (row, col, number) = get_input immutable_cells in
+    sudoku_grid.(row-1).(col-1) <- (number, (snd sudoku_grid.(row-1).(col-1)));
     run_game sudoku_grid (row+col == number) (move_count+1);
 
 (* Execute the print function to display the grid *)
