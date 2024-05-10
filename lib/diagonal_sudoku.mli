25cde081c6ac1f817090ddf5c054fbb2a7c22339
PeterFavero <pmf66@cornell.edu>
2024-05-10 14:33:02 -0400

    fix typo in diagonal_sucoku.mli

--- lib/diagonal_sudoku.mli
+++ lib/diagonal_sudoku.mli
@@ -44,7 +44,7 @@ val get_input : PairSet.t -> int * int * int
 (** Gets input from the user in the desired format and forbids changing of
     immutable blue cells *)
 
-val get_play_option : PairSet.t -> int * int * int
+val get_play_option : unit -> int
 (** Gets input from the user in the desired format as an integer 1-4, clearing the line and re-prompting otherwise *)
 
 val check_all_rows : int array array -> IntegerSet.t * IntegerSet.t
